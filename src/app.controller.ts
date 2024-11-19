7bcd81f252cd113899d24716e1a5de6bf2174fd7
Mariana Naselli <nasellimariana@outlook.com>
2024-11-19 23:10:48 +0100

    🛠️ package-lock.json -> Added Swagger and TSDoc packages 🛠️ package.json -> Updated dependencies for Swagger 🛠️ app.controller.ts -> Added Swagger decorators and endpoint 🛠️ main.ts -> Integrated Swagger setup for API documentation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags("AppController")
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get("/saludar")
+  @ApiOperation({ summary: 'Método get para probar la api' })
   getHello(): string {
     return this.appService.getHello();
   }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  //hola
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Configurar Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Portfolio API')
+    .setDescription('Documentación de la API del portfolio')
+    .setVersion('1.0')
+    .addTag('portfolio') // Opcional: Define un tag
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
 }
 bootstrap();
